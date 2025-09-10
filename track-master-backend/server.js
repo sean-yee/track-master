056e3a6a5dd3a014326668fc5f1a647771d99b29
Sean Yee <164131952+sean-yee@users.noreply.github.com>
2025-09-10 15:36:23 -0700

    Update server.js

--- track-master-backend/server.js
+++ track-master-backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const db = require("./database");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
