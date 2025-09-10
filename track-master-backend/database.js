56daa0627c4b3640896edaa8a3b054989de3a5dc
Sean Yee <164131952+sean-yee@users.noreply.github.com>
2025-09-10 15:34:38 -0700

    Update database.js

--- track-master-backend/database.js
+++ track-master-backend/database.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
 // Create or open the database
-const dbPath = path.resolve(__dirname, "leaderboard.db");
+const dbPath = path.resolve(__dirname, 'leaderboard.db');
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error("❌ Could not connect to database:", err.message);
